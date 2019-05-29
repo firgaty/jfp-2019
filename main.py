3f6f00f7910e3dcf593a21ce8c8af614b553460f
Peaupote <maxime.flin@gmail.com>
2019-05-29 12:05:27 +0200

    find 4 length sequences

--- main.py
+++ main.py
@@ -12,6 +12,30 @@ def play(c, p):
 
     board[6][c] = p
 
+def find_seq_aux(p, dx, dy):
+    seqs = []
+    for i in range(7):
+        for j in range(7):
+            s = []
+            for k in range(4):
+                x = j + k * dx
+                y = i + k * dy
+                if x < 7 and y < 7 and board[y][x] == p:
+                    s.append((x, y))
+                else:
+                    break
+            if k > 0:
+                seqs.append(s)
+    return seqs
+    
+def find_seq(p):
+    seqs = find_seq_aux(p, 1, 0)         # horiziontals
+    seqs.extend(find_seq_aux(p, 0, 1))  # verticals
+    seqs.extend(find_seq_aux(p, 1, 1))  # diags
+    seqs.extend(find_seq_aux(p, -1, 1)) # anti diags
+    seqs.sort(key=lambda x: len(x))
+    return seqs
+
 def cp_move():
     return 0
 
@@ -31,6 +55,9 @@ while(True):
         play(move, 2)
 
     pboard()
+
+    print("seq player 2")
+    print(find_seq(2))
         
     # we play
     move = cp_move()
